6826e5f65e0473defa159620972826e7ac855f98
KabutoDrip <cammiller42003@gmail.com>
2022-04-23 22:35:18 -0600

    ughhhhh

---
+++ lesson2/js/script.js
@@ -0,0 +1,6 @@
+const northman = document.getElementById('northman')
+const movie1info = document.getElementById('movie1id')
+
+northman.addEventListener('mouseover', () => container.style.backgroundColor = "blue")
+northman.addEventListener('mouseleave', () => container.style.backgroundColor = "brown")
+
