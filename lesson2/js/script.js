7b0062aadd8f0a3e4ca79f07704dce3cbb667387
KabutoDrip <cammiller42003@gmail.com>
2022-04-29 12:47:40 -0600

    Big Working Update

--- lesson2/js/script.js
+++ lesson2/js/script.js
@@ -1,6 +1,46 @@
-const northman = document.getElementById('northman')
-const movie1info = document.getElementById('movie1id')
 
-northman.addEventListener('mouseover', () => container.style.backgroundColor = "blue")
-northman.addEventListener('mouseleave', () => container.style.backgroundColor = "brown")
 
+if ( $(window).width() > 1023) {
+  function changeGridTemplateColumns(pattern) {
+    TweenMax.to(".container",
+      1, {
+        gridTemplateColumns: pattern
+      }
+    );
+  }
+  
+  
+  
+  $(document).ready(
+    function() {
+    
+  
+      $(".el-a,.el-d").mouseenter(
+        function() {
+          changeGridTemplateColumns("2fr 1fr 1fr");
+        }
+      );
+  
+      $(".el-b,.el-e").mouseenter(
+        function() {
+          changeGridTemplateColumns("1fr 2fr 1fr");
+        }
+      );
+  
+      $(".el-c,.el-f").mouseenter(
+        function() {
+          changeGridTemplateColumns("1fr 1fr 2fr");
+        }
+      );
+  
+      $(".container").mouseleave(
+        function() {
+          changeGridTemplateColumns("1fr 1fr 1fr");
+        }
+      );
+    }
+  );
+  }
+  else {
+  }
+ 
