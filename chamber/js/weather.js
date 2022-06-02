cb0dbeee5de12b36211c6fbf0f72fdbff3d46fd9
KabutoDrip <cammiller42003@gmail.com>
2022-06-02 12:14:31 -0600

    lazyloading

--- chamber/js/weather.js
+++ chamber/js/weather.js
@@ -1,17 +1,17 @@
 var city = document.querySelector(".city");
 var wspeed = document.querySelector(".wspeed");
 var temperature = document.querySelector(".temperature");
-
+var windchill = document.querySelector(".chill");
 fetch("https://api.openweathermap.org/data/2.5/weather?q=Rexburg&appid=2a862fd172efcdfbe3b4a778bbdbf229")
   .then(response => response.json())
   .then(data => {
     var nameValue = "Hobbiton";
-    var tempValue = "Temperaure: " + data['main']['temp'];
-    var wspeedValue = "Wind Speed: " + data['wind']['speed'];
-    var wchillValue = "Wind Chill: " + `${(35.74 + (tempValue * 0.6215) - (35.75 * (wspeedValue**16)))}`;
+    var tempValue = parseFloat(data['main']['temp']);
+    var wspeedValue = parseFloat(data['wind']['speed']);
+    var wchillValue = (35.74 + (tempValue * 0.6215) - (35.75 * (wspeedValue**16)));
     city.innerHTML = nameValue;
-    temperature.innerHTML = tempValue;
-    wspeed.innerHTML = wspeedValue;
-    chill.innerHTML = wchillValue;
+    temperature.innerHTML = "Temperaure: " + tempValue;
+    wspeed.innerHTML = "Wind Speed: " +  wspeedValue;
+    chill.innerHTML = "Wind Chill: " + wchillValue;
 
   })
--- chamber/js/windchill.js
+++
@@ -1,5 +0,0 @@
-var temp = document.querySelector(".temperature");
-var wispeed = document.querySelector(".wspeed");
-var chill = document.querySelector(".chill");
-var calcchill = (35.74 + (.6215 * temp) - (35.75 * (wispeed**16)));
-chill.innerHTML = calcchill;
