21fe37f02e837a41d00b0ef1a2662b72808a76ab
Camden Miller <91078110+KabutoDrip@users.noreply.github.com>
2022-06-14 10:06:22 -0600

    fixed dom manipulation assignment and added hidden time tracker for join page

--- bom/bom.js
+++ bom/bom.js
@@ -4,9 +4,16 @@ var list
 
 function appendList() {
 	var input = document.getElementById("favchap").value;
-
+	var button = document.createElement("button");
 	var ul = document.getElementById("list");
 	var li = document.createElement("li");
+	button.innerHTML = "❌";
 	li.appendChild(document.createTextNode(input));
+	li.appendChild(button)
 	ul.appendChild(li);
+	button.addEventListener("click", function (){
+		li.parentNode.removeChild(li);
+
+	})
+
 }
