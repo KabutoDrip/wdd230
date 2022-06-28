579c7347d6826bae9557fcf2d4fc771d241733e2
Camden Miller <91078110+KabutoDrip@users.noreply.github.com>
2022-06-28 10:17:19 -0600

    weather data broken css

--- chamber/js/weather.js
+++ chamber/js/weather.js
@@ -2,6 +2,8 @@ var city = document.querySelector(".city");
 var wspeed = document.querySelector(".wspeed");
 var temperature = document.querySelector(".temperature");
 var windchill = document.querySelector(".chill");
+var weatherIcon = document.querySelector(".wicon")
+var captiondesc = document.querySelector("figcaption")
 fetch("https://api.openweathermap.org/data/2.5/weather?q=Rexburg&appid=2a862fd172efcdfbe3b4a778bbdbf229")
   .then(response => response.json())
   .then(data => {
@@ -18,9 +20,10 @@ fetch("https://api.openweathermap.org/data/2.5/weather?q=Rexburg&appid=2a862fd17
     else{
       windchill.innerHTML = "Wind Chill: None" 
     }
-  })
-  var iconsrc = `https://openweathermap.org/img/w/${data.weather[0].icon}.png`;
-  var desc = data.weather[0].description;
+    var iconsrc = `https://openweathermap.org/img/wn/${data.weather[0].icon}@4x.png`;
+    var desc = data.weather[0].description;
+    weatherIcon.setAttribute('src', iconsrc);weatherIcon.setAttribute('alt', desc);
+    captiondesc.textContent = desc;
 
-  weatherIcon.setAttribute('src', iconsrc);weatherIcon.setAttribute('alt', desc);
-  captionDesc.textContent = desc;
+  })
+
